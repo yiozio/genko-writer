b1b7cd89e949792ee9ceb2ef80604ed19eb28276
Yuzo <yoozoo.mng@gmail.com>
2020-03-09 22:33:38 +0900

    safariで２行目以降の入力文字が消える問題の対処

--- src/GenkoEditor.tsx
+++ src/GenkoEditor.tsx
@@ -9,18 +9,54 @@ type Props = {
 type DomProps = Props & {
   className?: string;
   domRef: React.RefObject<HTMLDivElement>;
+  inComposition: boolean;
+  onCompositionStart: () => void;
+  onCompositionEnd: () => void;
 };
 
-const Dom = ({ className, domRef, onInput, onResize }: DomProps) => (
+const Dom = ({
+  className,
+  domRef,
+  onInput,
+  onResize,
+  inComposition,
+  onCompositionStart,
+  onCompositionEnd
+}: DomProps) => (
   <div
     className={className}
     ref={domRef}
     contentEditable
+    onKeyDown={e => {
+      if (e.key !== 'Enter' || inComposition) return;
+      const s = window.getSelection();
+      if (s) {
+        s.deleteFromDocument();
+        const r = s.getRangeAt(0);
+        if (e.currentTarget.lastChild?.nodeName !== 'BR') {
+          e.currentTarget.append(document.createElement('br'));
+        }
+        const br = document.createElement('br');
+        r.insertNode(br);
+        r.setStartAfter(br);
+        r.setEndAfter(br);
+        r.collapse(false);
+        s.removeAllRanges();
+        s.addRange(r);
+      }
+      e.preventDefault();
+    }}
+    onCompositionStart={onCompositionStart}
+    onCompositionEnd={onCompositionEnd}
     onInput={e => {
-      let text: string[] = [];
+      let text: string[] = [''];
       for (let i = 0; i < e.currentTarget.childNodes.length; i += 1) {
         const child = e.currentTarget.childNodes.item(i);
-        text.push(child.textContent || '');
+        if (child.nodeName === 'BR') {
+          text.push('');
+        } else {
+          text[text.length - 1] += child.textContent || '';
+        }
       }
       onResize(e.currentTarget.clientWidth);
       onInput(text.join('\n'));
@@ -45,17 +81,28 @@ const Styled = styled(Dom)({
 
 export default function GenkoEditor({ initText, onInput, onResize }: Props) {
   const domRef = React.useRef<HTMLDivElement>(null);
+  const [inComposition, setInComposition] = React.useState(false);
 
   React.useEffect(() => {
-    if (domRef && domRef.current) {
+    if (domRef.current) {
       domRef.current.innerHTML =
         initText
           ?.split('\n')
-          ?.map(t => `<div>${t || '<br>'}</div>`)
+          ?.map(t => `${t || ''}<br>`)
           .join('') || '';
       onResize(domRef.current.clientWidth);
     }
   }, []);
 
-  return <Styled domRef={domRef} initText={initText} onInput={onInput} onResize={onResize} />;
+  return (
+    <Styled
+      domRef={domRef}
+      initText={initText}
+      onInput={onInput}
+      onResize={onResize}
+      inComposition={inComposition}
+      onCompositionStart={() => setInComposition(true)}
+      onCompositionEnd={() => setInComposition(false)}
+    />
+  );
 }
