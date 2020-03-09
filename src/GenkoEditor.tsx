8a91b243d4cb8e9d947a7484d6005e29a8619954
Yuzo <yoozoo.mng@gmail.com>
2020-03-10 02:40:53 +0900

    矢印キーによるカーソル移動を縦書き用にカスタマイズ

--- src/GenkoEditor.tsx
+++ src/GenkoEditor.tsx
@@ -28,23 +28,60 @@ const Dom = ({
     ref={domRef}
     contentEditable
     onKeyDown={e => {
-      if (e.key !== 'Enter' || inComposition) return;
-      const s = window.getSelection();
-      if (s) {
-        s.deleteFromDocument();
-        const r = s.getRangeAt(0);
-        if (e.currentTarget.lastChild?.nodeName !== 'BR') {
-          e.currentTarget.append(document.createElement('br'));
+      if (e.key === 'Enter') {
+        if (inComposition) return;
+        const s = window.getSelection();
+        if (s) {
+          s.deleteFromDocument();
+          const r = s.getRangeAt(0);
+          if (e.currentTarget.lastChild?.nodeName !== 'BR') {
+            e.currentTarget.append(document.createElement('br'));
+          }
+          const br = document.createElement('br');
+          r.insertNode(br);
+          r.setStartAfter(br);
+          r.setEndAfter(br);
+          r.collapse(false);
+          s.removeAllRanges();
+          s.addRange(r);
         }
-        const br = document.createElement('br');
-        r.insertNode(br);
-        r.setStartAfter(br);
-        r.setEndAfter(br);
-        r.collapse(false);
-        s.removeAllRanges();
-        s.addRange(r);
+        e.preventDefault();
+      } else if (e.key.substring(0, 5) === 'Arrow') {
+        const s = window.getSelection();
+        const anchor = s?.anchorNode;
+        if (s && anchor) {
+          const r = s.getRangeAt(0);
+          let node = anchor;
+          let offset = s.anchorOffset;
+          if (e.key === 'ArrowUp') {
+            if (offset > 0) {
+              offset -= 1;
+            } else {
+              node = anchor.previousSibling || anchor;
+              if (node !== anchor) offset = node.textContent?.length || 0;
+            }
+          } else if (e.key === 'ArrowDown') {
+            const length = node.textContent?.length || 0;
+            if (offset < length) {
+              offset += 1;
+            } else {
+              node = anchor.nextSibling || anchor;
+              if (node !== anchor) offset = 0;
+            }
+          } else if (e.key === 'ArrowLeft') {
+            node = anchor.nextSibling || anchor;
+            offset = node !== anchor ? 0 : node.textContent?.length || 0;
+          } else {
+            node = anchor.previousSibling || anchor;
+            offset = 0;
+          }
+          r.setStart(node, offset);
+          r.setEnd(node, offset);
+          s.removeAllRanges();
+          s.addRange(r);
+        }
+        e.preventDefault();
       }
-      e.preventDefault();
     }}
     onCompositionStart={onCompositionStart}
     onCompositionEnd={onCompositionEnd}
